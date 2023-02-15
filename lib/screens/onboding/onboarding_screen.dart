e510305a956a252d92dc4a7261d4ce31f44218ea
Asish Mahapatra <asishmahapatra918@gmail.com>
2023-02-15 22:50:35 +0530

    Added Sign In dialog box with Animations

--- lib/screens/onboding/onboarding_screen.dart
+++ lib/screens/onboding/onboarding_screen.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:rive/rive.dart';
 
 import '../components/animated_btn.dart';
+import '../components/custom_sign_in_dialog.dart';
 
 class OnboardingScreen extends StatefulWidget {
   const OnboardingScreen({super.key});
@@ -13,6 +14,7 @@ class OnboardingScreen extends StatefulWidget {
 }
 
 class _OnboardingScreenState extends State<OnboardingScreen> {
+  bool isSignInDialogShown = false;
   late RiveAnimationController _btnAnimationController;
 
   @override
@@ -42,46 +44,68 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
               child: const SizedBox(),
             ),
           ),
-          SafeArea(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 32),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const Spacer(),
-                  SizedBox(
-                    width: 260,
-                    child: Column(
-                      children: const [
-                        Text(
-                          "Learn design & code",
-                          style: TextStyle(
-                            fontSize: 60,
-                            fontFamily: "Poppins",
-                            height: 1.2,
+          AnimatedPositioned(
+            top: isSignInDialogShown ? -50 : 0,
+            duration: const Duration(milliseconds: 240),
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
+            child: SafeArea(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 32),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Spacer(),
+                    SizedBox(
+                      width: 260,
+                      child: Column(
+                        children: const [
+                          Text(
+                            "Learn design & code",
+                            style: TextStyle(
+                              fontSize: 60,
+                              fontFamily: "Poppins",
+                              height: 1.2,
+                            ),
                           ),
-                        ),
-                        SizedBox(
-                          height: 16,
-                        ),
-                        Text(
-                            "Don't skip design. Learn design and code by building real apps with FLutter and Swift. Complete courses about the best tools.")
-                      ],
+                          SizedBox(
+                            height: 16,
+                          ),
+                          Text(
+                              "Don't skip design. Learn design and code by building real apps with Flutter and Swift. Complete courses about the best tools.")
+                        ],
+                      ),
                     ),
-                  ),
-                  const Spacer(flex: 2),
-                  AnimatedBtn(
-                    btnAnimationController: _btnAnimationController,
-                    press: () {
-                      _btnAnimationController.isActive = true;
-                    },
-                  ),
-                  const Padding(
-                    padding: EdgeInsets.symmetric(vertical: 24),
-                    child: Text(
-                        "Purchase includes access to 30+ courses. 240+ premium tutorials, 120+ hours of videos, source files and certificates."),
-                  )
-                ],
+                    const Spacer(flex: 2),
+                    AnimatedBtn(
+                      btnAnimationController: _btnAnimationController,
+                      press: () {
+                        _btnAnimationController.isActive = true;
+                        Future.delayed(
+                          const Duration(milliseconds: 800),
+                          () {
+                            setState(() {
+                              isSignInDialogShown = true;
+                            });
+                            customSignInDialog(
+                              context,
+                              onClosed: (_) {
+                                setState(() {
+                                  isSignInDialogShown = false;
+                                });
+                              },
+                            );
+                          },
+                        );
+                      },
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.symmetric(vertical: 24),
+                      child: Text(
+                          "Purchase includes access to 30+ courses. 240+ premium tutorials, 120+ hours of videos, source files and certificates."),
+                    )
+                  ],
+                ),
               ),
             ),
           )
