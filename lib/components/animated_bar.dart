9f7c2de640062939e37c89271e972b0398d8bd82
Asish Mahapatra <asishmahapatra918@gmail.com>
2023-02-22 23:34:59 +0530

    Made the Home Page of the app with various Cards

--- lib/components/animated_bar.dart
+++ lib/components/animated_bar.dart
@@ -1,26 +1,26 @@
 import 'package:flutter/material.dart';
 
-import '../screens/entry_point.dart';
-
 class AnimatedBar extends StatelessWidget {
   const AnimatedBar({
     Key? key,
     required this.isActive,
-  }) :  super(key: key);
+  }) : super(key: key);
 
   final bool isActive;
 
   @override
   Widget build(BuildContext context) {
     return AnimatedContainer(
-                      duration: Duration(milliseconds: 200),
-                      margin: EdgeInsets.only(bottom: 2),
-                      height: 4,
-                      width: isActive ? 20 : 0,
-                      decoration: BoxDecoration(
-                        color: Color(0xFF81B4FF),
-                        borderRadius: BorderRadius.all(Radius.circular(12),), 
-                      ),
-                    );
+      duration: const Duration(milliseconds: 200),
+      margin: const EdgeInsets.only(bottom: 2),
+      height: 4,
+      width: isActive ? 20 : 0,
+      decoration: const BoxDecoration(
+        color: Color(0xFF81B4FF),
+        borderRadius: BorderRadius.all(
+          Radius.circular(12),
+        ),
+      ),
+    );
   }
-  }
+}
--- lib/screens/entry_point.dart
+++ lib/screens/entry_point.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:rive/rive.dart';
 import 'package:rive_animation/constants.dart';
+import 'package:rive_animation/screens/home_screen.dart';
 import 'package:rive_animation/utils/rive_utils.dart';
 
 import '../components/animated_bar.dart';
@@ -14,13 +15,15 @@ class EntryPoint extends StatefulWidget {
 }
 
 class _EntryPointState extends State<EntryPoint> {
-
   RiveAsset selectedBottomNav = bottomNavs.first;
 
   late SMIBool searchTrigger;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
+      extendBody: true,
+      body: const HomeScreen(),
       bottomNavigationBar: SafeArea(
         child: Container(
           padding: const EdgeInsets.all(12),
@@ -37,9 +40,9 @@ class _EntryPointState extends State<EntryPoint> {
                   onTap: () {
                     bottomNavs[index].input!.change(true);
                     if (bottomNavs[index] != selectedBottomNav) {
-                        setState(() {
-                          selectedBottomNav = bottomNavs[index];
-                        });
+                      setState(() {
+                        selectedBottomNav = bottomNavs[index];
+                      });
                     }
                     Future.delayed(const Duration(seconds: 1), () {
                       bottomNavs[index].input!.change(false);
@@ -48,12 +51,14 @@ class _EntryPointState extends State<EntryPoint> {
                   child: Column(
                     mainAxisSize: MainAxisSize.min,
                     children: [
-                      AnimatedBar(isActive: bottomNavs[index] == selectedBottomNav),
+                      AnimatedBar(
+                          isActive: bottomNavs[index] == selectedBottomNav),
                       SizedBox(
                         height: 36,
                         width: 36,
                         child: Opacity(
-                          opacity: bottomNavs[index] == selectedBottomNav ? 1 : 0.5,
+                          opacity:
+                              bottomNavs[index] == selectedBottomNav ? 1 : 0.5,
                           child: RiveAnimation.asset(
                             bottomNavs.first.src,
                             artboard: bottomNavs[index].artboard,
@@ -62,7 +67,7 @@ class _EntryPointState extends State<EntryPoint> {
                                   RiveUtils.getRiveController(artboard,
                                       stateMachineName:
                                           bottomNavs[index].stateMachineName);
-                        
+
                               bottomNavs[index].input =
                                   controller.findSMI("active") as SMIBool;
                             },
